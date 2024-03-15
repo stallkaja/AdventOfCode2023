777a99099efac918adb5de8c6e1a3f0d4f293c8f
Stallkamp <James.Stallkamp@lamresearch.com>
2024-03-15 06:33:54 -0700

    adding day 8 part A completed

--- 8/8a.py
+++ 8/8a.py
@@ -4,28 +4,34 @@ import sys
 def main():
     with open('input.txt') as f:
         lines = [line.rstrip('\n') for line in f]
-
-
-    instructions = lines[0]
-    lines.pop(0) #removing instructions string from lines
-    lines.pop(0) #removing extra space from lines
+    temp = lines[0]
+    instructions = []
+    lines.pop(0)  # removing instructions string from lines
+    lines.pop(0)  # removing extra space from lines
     lines = [i.split('=') for i in lines]
-    print(lines)
-    print(lines[0][1].split(', '))
+    for ele in temp:
+        instructions.append(ele)
+    map = {}
+    for line in lines:
+        map[line[0][:-1]] = ((line[1].split(', '))[0][2:],(line[1].split(', '))[1][:-1])
 
-    #for instruction in instructions:
-        #print(instruction)
-    #for line in lines:
-        #print(line)
-    #print(lines)
+    cur = 'AAA'
+    count = 0
+    i = 0
+    while cur != 'ZZZ':
+        if (i >= len(instructions)):
+            i = 0
+        if (instructions[i] == "L"):
+            cur = map[cur][0]
+            count += 1
+        elif (instructions[i] == "R"):
+            cur = map[cur][1]
+            count += 1
+        else:
+            print("error")
+        i += 1
+    print("ZZZ has been found")
+    print(count)
 
-    #target = 'AAA'
-    #test = [item for item in lines if item[1] == target]
-    #for item in lines:
-        #print(item[1])
-    #print(test)
-    #print(sorted(lines))
-    #print((sorted(lines)[0][1]).split(','))
-    #print(lines)
 if __name__ == "__main__":
     main()
